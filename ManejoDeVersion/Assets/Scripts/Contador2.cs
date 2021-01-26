59cacb861a50767b0b76810b51627de96dcdc4f8
Cris0904 <cristian.munozh@upb.edu.co>
2021-01-25 20:44:56 -0500

    Second script class done

---
+++ ManejoDeVersion/Assets/Scripts/Contador2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contador2 : MonoBehaviour
+{
+    public int conteo;
+    public bool funcionar;
+
+    public Text contador;
+    void Start()
+    {
+        conteo = 0;
+        funcionar = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        contador.text = conteo.ToString();
+    }
+
+    public void sumar()
+    {
+        if (funcionar)
+        {
+            conteo = conteo + 1;
+        }
+    }
+}
