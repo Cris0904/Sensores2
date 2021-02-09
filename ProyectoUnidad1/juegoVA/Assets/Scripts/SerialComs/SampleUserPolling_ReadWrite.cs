416d1031f9666ee76702787101fd3f5383691271
Cris0904 <cristian.munozh@upb.edu.co>
2021-02-09 09:33:02 -0500

    ComsUpdate-Proj-U1

--- ProyectoUnidad1/juegoVA/Assets/Scripts/SerialComs/SampleUserPolling_ReadWrite.cs
+++ ProyectoUnidad1/juegoVA/Assets/Scripts/SerialComs/SampleUserPolling_ReadWrite.cs
@@ -10,9 +10,9 @@ using UnityEngine;
 using System.Collections;
 
 /**
- * Sample for reading using polling by yourself. In case you are fond of that.
+ * Sample for reading using polling by yourself, and writing too.
  */
-public class SampleUserPolling_JustRead : MonoBehaviour
+public class SampleUserPolling_ReadWrite : MonoBehaviour
 {
     public SerialController serialController;
 
@@ -20,16 +20,40 @@ public class SampleUserPolling_JustRead : MonoBehaviour
     void Start()
     {
         serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
-	}
+
+        Debug.Log("Press A or Z to execute some actions");
+    }
 
     // Executed each frame
     void Update()
     {
+        //---------------------------------------------------------------------
+        // Send data
+        //---------------------------------------------------------------------
+
+        // If you press one of these keys send it to the serial device. A
+        // sample serial device that accepts this input is given in the README.
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("Sending A");
+            serialController.SendSerialMessage("A");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Debug.Log("Sending Z");
+            serialController.SendSerialMessage("Z");
+        }
+
+
+        //---------------------------------------------------------------------
+        // Receive data
+        //---------------------------------------------------------------------
+
         string message = serialController.ReadSerialMessage();
 
         if (message == null)
             return;
-        Debug.Log(message);
 
         // Check if the message is plain data or a connect/disconnect event.
         if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
